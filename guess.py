ce9820b4511a722c5a1305ff5b78d9242cc5f077
kethashishir <kethashishir@gmail.com>
2026-02-10 08:54:12 -0800

    Copy the files to main directory

---
+++ guess.py
@@ -0,0 +1,19 @@
+﻿secret_number = 777
+
+print(
+"""
++================================+
+| Welcome to my game, muggle!    |
+| Enter an integer number        |
+| and guess what number I've     |
+| picked for you.                |
+| So, what is the secret number? |
++================================+
+""")
+while(True):
+    guess=int(input("Guess a number:"))
+    if guess==secret_number:
+        print("Well done, muggle! You are free now.")
+        break
+    else:
+        print("Ha ha! you're stuck in my loop!")
---
+++ test.py
@@ -0,0 +1,8 @@
+x=int(input("enterx:"))
+y=int(input("entery:"))
+x=x%y
+print(x)
+x=x%y
+print(x)
+y=y%x
+print(y)
